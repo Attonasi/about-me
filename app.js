9193a214d43e6a0bcc382134d0e4540ea051cd75
Attonasi <attonasi@hotmail.com>
2016-11-29 14:43:39 -0800

    modified app.js and index.html

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-alert('I AM ALIVE!');
+'use strict';
+
+//The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+
+var question1 = prompt('Does Collin have any cats?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y'){
+  alert ('you are incorrect I dont have no stinking cats,');
+}else if(question1 ==='no' || question1 === 'n'){
+  alert:('You are right I have no cats.');
+}else{
+  alert('yes or no jerk.');
+}
+var question1 = prompt('Does Collin have a job?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y'){
+  alert ('you are incorrect does not have a stupid job.');
+}else if(question1 ==='no' || question1 === 'n'){
+  alert:('You are right I have no job.');
+}else{
+  alert('yes or no jerk.');
+}
+
+var question1 = prompt('Does Collin have any idea what he is doing?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y'){
+  alert ('you are incorrect I dont have no idea what is happening');
+}else if(question1 ==='no' || question1 === 'n'){
+  alert:('You are right I have no clue.');
+}else{
+  alert('yes or no jerk.');
+}
+
+var question1 = prompt('Does Collin have any cats?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y'){
+  alert ('you are incorrect I dont have no stinking cats,');
+}else if(question1 ==='no' || question1 === 'n'){
+  alert:('You are right I have no cats.');
+}else{
+  alert('yes or no jerk.');
+}
+
+var question1 = prompt('Does Collin have any cats?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y'){
+  alert ('you are incorrect I dont have no stinking cats,');
+}else if(question1 ==='no' || question1 === 'n'){
+  alert:('You are right I have no cats.');
+}else{
+  alert('yes or no jerk.');
+}
