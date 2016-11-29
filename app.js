ab8c31e29dff5e315a6f951a49f89adb816b644e
Attonasi <attonasi@hotmail.com>
2016-11-29 14:55:26 -0800

    modified app.js

--- app.js
+++ app.js
@@ -2,50 +2,50 @@
 
 //The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
 
-var question1 = prompt('Does Collin have any cats?').toLowerCase();
+var response1 = prompt('Does Collin have any cats?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y'){
+if (response1 === 'yes' || response1 === 'y'){
   alert ('you are incorrect I dont have no stinking cats,');
-}else if(question1 ==='no' || question1 === 'n'){
+}else if(response1 ==='no' || response1 === 'n'){
   alert:('You are right I have no cats.');
 }else{
   alert('yes or no jerk.');
 }
-var question1 = prompt('Does Collin have a job?').toLowerCase();
+var response2 = prompt('Does Collin have a job?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y'){
+if (response2 === 'yes' || response2 === 'y'){
   alert ('you are incorrect does not have a stupid job.');
-}else if(question1 ==='no' || question1 === 'n'){
+}else if(response2 ==='no' || response2 === 'n'){
   alert:('You are right I have no job.');
 }else{
   alert('yes or no jerk.');
 }
 
-var question1 = prompt('Does Collin have any idea what he is doing?').toLowerCase();
+var response3 = prompt('Does Collin have any idea what he is doing?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y'){
+if (response3 === 'yes' || response3 === 'y'){
   alert ('you are incorrect I dont have no idea what is happening');
-}else if(question1 ==='no' || question1 === 'n'){
+}else if(response3 ==='no' || response3 === 'n'){
   alert:('You are right I have no clue.');
 }else{
   alert('yes or no jerk.');
 }
 
-var question1 = prompt('Does Collin have any cats?').toLowerCase();
+var response4 = prompt('Does Collin have any cats?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y'){
+if (response4 === 'yes' || response4 === 'y'){
   alert ('you are incorrect I dont have no stinking cats,');
-}else if(question1 ==='no' || question1 === 'n'){
+}else if(response4 ==='no' || response4 === 'n'){
   alert:('You are right I have no cats.');
 }else{
   alert('yes or no jerk.');
 }
 
-var question1 = prompt('Does Collin have any cats?').toLowerCase();
+var response5 = prompt('Does Collin have any cats?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y'){
+if (response5 === 'yes' || response5 === 'y'){
   alert ('you are incorrect I dont have no stinking cats,');
-}else if(question1 ==='no' || question1 === 'n'){
+}else if(response5 ==='no' || response5 === 'n'){
   alert:('You are right I have no cats.');
 }else{
   alert('yes or no jerk.');
